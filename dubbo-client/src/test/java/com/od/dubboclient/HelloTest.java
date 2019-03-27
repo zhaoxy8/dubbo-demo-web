a27cb1bf43b48e10a2f8fdc71eb883ebf939ec59
StanleyWang <stanley.wang.m@qq.com>
2019-03-27 18:14:58 +0800

    Update HelloTest.java

--- dubbo-client/src/test/java/com/od/dubboclient/HelloTest.java
+++ dubbo-client/src/test/java/com/od/dubboclient/HelloTest.java
@@ -20,7 +20,7 @@ public class HelloTest {
 
 	@Before
 	public void setUp() throws Exception {
-		this.url = new URL("http://localhost:9090/hello?name={name}");
+		this.url = new URL("http://localhost:8080/hello?name={name}");
 		rest = new TestRestTemplate();
 	}
 
