cec342cfbe9a194794ebf348ddadc227644d9ecf
StanleyWang-GitHub <stanley.wang@live.cn>
2019-04-08 15:15:40 +0800

    tomcat supported.

--- dubbo-client/src/main/java/com/od/dubbotest/Application.java
+++ dubbo-client/src/main/java/com/od/dubbotest/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.od.dubbotest.action.HelloAction;
 
