87b3ea4278ef95cb39e89cc28411ce29051e6685
StanleyWang-GitHub <stanley.wang@live.cn>
2019-04-09 09:44:45 +0800

    modify HelloAction.java

--- dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -15,7 +15,7 @@ public class HelloAction {
 	@RequestMapping
 	public String say(String name) {
 		System.out.println("HelloAction接收到请求:"+name);
-		String str="<h1>这是Dubbo 消费者端</h1>";
+		String str="<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str+=helloService.hello(name);
 		System.out.println("HelloService返回到结果:"+str);
 		return str;
