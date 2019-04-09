9af53569097f1ded997c9a241244caabb500b6b9
StanleyWang-GitHub <stanley.wang@live.cn>
2019-04-09 09:39:58 +0800

    support tomcat.

--- dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -19,7 +19,7 @@ public class HelloAction {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
 		String ds = df.format(new Date());
 		System.out.println(ds + "HelloAction接收到请求:"+name);
-		String str = ds + "<h1>这是Dubbo 消费者端</h1>";
+		String str = ds + "<h1>这是Dubbo 消费者端(Tomcat服务)</h1>";
 		str+=helloService.hello(name);
 		System.out.println(ds + "HelloService返回到结果:"+str);
 		return str;
