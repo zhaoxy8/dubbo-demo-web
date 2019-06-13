066d997ae8229a64af7913ccb6dce3ad865609a2
StanleyWang <stanley.wang.m@qq.com>
2019-06-13 11:28:28 +0800

    Update Application.java

--- dubbo-client/src/main/java/com/od/dubbotest/Application.java
+++ dubbo-client/src/main/java/com/od/dubbotest/Application.java
@@ -11,8 +11,7 @@ import com.od.dubbotest.action.BjAction;
 public class Application {
     public static void main( String[] args) {
     	ApplicationContext context=SpringApplication.run("classpath*:spring-config.xml",args);
-    	//HelloAction action=(HelloAction) context.getBean("helloAction");
-        BjAction action=(BjAction) context.getBean("bjAction");
+    	HelloAction action=(HelloAction) context.getBean("helloAction");
         System.out.println("Dubbo client started");
         System.out.println("Dubbo 消费者端启动");
         //action.say("jack");
