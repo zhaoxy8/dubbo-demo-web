411d853de59a7a33bcf9c56ebc4f7c3fa75a775f
StanleyWang <stanley.wang.m@qq.com>
2019-10-22 18:18:56 +0800

    修改

--- dubbo-client/src/main/java/com/od/dubbotest/Application.java
+++ dubbo-client/src/main/java/com/od/dubbotest/Application.java
@@ -13,7 +13,7 @@ public class Application {
     public static void main( String[] args) {
     	ApplicationContext context=SpringApplication.run("classpath*:spring-config.xml",args);
     	HelloAction action=(HelloAction) context.getBean("helloAction");
-        ShAction bjAction=(BjAction) context.getBean("bjAction");
+        BjAction bjAction=(BjAction) context.getBean("bjAction");
         ShAction shAction=(ShAction) context.getBean("shAction");
         System.out.println("Dubbo client started");
         System.out.println("Dubbo 消费者端启动");
