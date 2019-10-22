192a687d8e01ba87e6f1ebfff9f4e7b81a75284a
StanleyWang <stanley.wang.m@qq.com>
2019-10-22 16:23:03 +0800

    修改

--- dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
@@ -15,7 +15,7 @@ public class ShAction {
 	@RequestMapping
 	public String say(String name) {
 		System.out.println("HelloAction接收到请求:"+name);
-		String str="<h1>这是Dubbo 消费者端(springboot)</h1>";
+		String str="<h1>这是Dubbo 消费者端(tomcat)</h1>";
 		str+="<h2>这里是上海站的页面</h2>";
 		str+=helloService.hello(name);
 		System.out.println("HelloService返回到结果:"+str);
