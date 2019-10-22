261084b75b3230b69fa305f19b6b8a3339d7b58a
StanleyWang <stanley.wang.m@qq.com>
2019-10-22 16:11:10 +0800

    新增灰度发布，北京站的内容

--- dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
@@ -17,7 +17,6 @@ public class BjAction {
 		System.out.println("HelloAction接收到请求:"+name);
 		String str="<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str+="<h2>这里是北京站的页面</h2>";
-        str+="<h2>这里是新加入的功能。。。</h2>";
 		str+=helloService.hello(name);
 		System.out.println("HelloService返回到结果:"+str);
 		return str;
