b2500cf41741051af54c6ef92ecde8c2d33b62e7
StanleyWang <stanley.wang.m@qq.com>
2019-12-29 18:44:33 +0800

    update dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java.

--- dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -20,7 +20,7 @@ public class HelloAction {
 		String ds = df.format(new Date());
 		System.out.println(ds + "HelloAction接收到请求:"+name);
 		String str = ds + "<h1>这是Dubbo 消费者端(Tomcat服务)</h1>";
-        str += "<h2>欢迎来到老男孩教育K8S容器云架构师专题课培训讲座。</h2>";
+        str += "<h2>欢迎来到老男孩教育K8S容器云架构师专题课培训班1期。</h2>";
 		str += helloService.hello(name);
 		System.out.println(ds + "HelloService返回到结果:"+str);
 		return str;
