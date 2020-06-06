8652972e6b01c4f7f3cd1942a13c191526231178
xy1219.zhao <xy1219.zhao@samsung.com>
2020-06-06 07:06:54 +0000

    增加slf4j配置 添加marker

--- dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
@@ -2,11 +2,8 @@ package com.od.dubbotest.action;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.od.dubbotest.api.HelloService;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,12 +16,13 @@ public class BjAction {
 	@RequestMapping
 	public String say(String name) {
 	        Logger log = LoggerFactory.getLogger(BjAction.class);
-		log.info("HelloAction接收到请求:", name);
+                String marker = "BjAction";
+		log.info(marker, "HelloAction接收到请求:", name);
 		String str = "<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str += "<h2>这里是北京站的页面</h2>";
         str+="<h2>这里是北京站新加入的功能，上海灰度测试后投产。</h2>";
 		str += helloService.hello(name);
-		log.info("HelloService返回到结果:", str);
+		log.info(marker, "HelloService返回到结果:", str);
 		return str;
 	}
 }
--- dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
@@ -17,12 +17,13 @@ public class ShAction {
 
 	public String say(String name) {
                 Logger log = LoggerFactory.getLogger(ShAction.class);
-		log.info("HelloAction接收到请求:", name);
+                String marker = "ShAction";
+		log.info(marker, "HelloAction接收到请求:", name);
 		String str = "<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str += "<h2>这里是上海站的页面</h2>";
-        str+="<h2>这里是上海站新加入的功能，灰度测试</h2>";
+                str+="<h2>这里是上海站新加入的功能，灰度测试</h2>";
 		str += helloService.hello(name);
-		log.info("HelloService返回到结果:", str);
+		log.info(marker, "HelloService返回到结果:", str);
 		return str;
 	}
 }
