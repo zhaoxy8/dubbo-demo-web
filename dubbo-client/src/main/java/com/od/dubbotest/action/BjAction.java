937a48f199debe0191b7fea53bcf6dd8deefc219
xy1219.zhao <xy1219.zhao@samsung.com>
2020-06-06 06:51:35 +0000

    增加slf4j配置

--- dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
@@ -19,12 +19,12 @@ public class BjAction {
 	@RequestMapping
 	public String say(String name) {
 	        Logger log = LoggerFactory.getLogger(BjAction.class);
-		log.info("HelloAction接收到请求:" + name);
+		log.info("HelloAction接收到请求:", name);
 		String str = "<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str += "<h2>这里是北京站的页面</h2>";
         str+="<h2>这里是北京站新加入的功能，上海灰度测试后投产。</h2>";
 		str += helloService.hello(name);
-		log.info("HelloService返回到结果:" + str);
+		log.info("HelloService返回到结果:", str);
 		return str;
 	}
 }
--- dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -23,10 +23,10 @@ public class HelloAction {
 	        Logger log = LoggerFactory.getLogger(HelloAction.class);
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
 		String ds = df.format(new Date());
-                log.info(ds + "HelloAction接收到请求:" + name);
+                log.info(ds, "HelloAction接收到请求:", name);
 		String str = ds + "<h1>这是Dubbo 消费者端(Apollo)</h1><h2>o(*￣︶￣*)o欢迎来到老男孩教育K8S容器云架构师专题课1期。</h2>";
 		str+=helloService.hello(name);
-                log.info(ds + "HelloService返回到结果:" + str);
+                log.info(ds, "HelloService返回到结果:", str);
 		return str;
 	}
 }
