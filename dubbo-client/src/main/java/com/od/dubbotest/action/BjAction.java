1a5a29d78f3b66fe8626b9e192554228f756375a
xy1219.zhao <xy1219.zhao@samsung.com>
2020-06-06 07:50:24 +0000

    log.info(Level.INFO)

--- dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/BjAction.java
@@ -15,12 +15,12 @@ public class BjAction {
 	@RequestMapping
 	public String say(String name) {
 	        Logger log = Logger.getLogger("com");
-		log.info(Level.INFO, "HelloAction接收到请求:", name);
+		log.log(Level.INFO, "HelloAction接收到请求:" + name);
 		String str = "<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str += "<h2>这里是北京站的页面</h2>";
         str+="<h2>这里是北京站新加入的功能，上海灰度测试后投产。</h2>";
 		str += helloService.hello(name);
-		log.info(Level.INFO, "HelloService返回到结果:", str);
+		log.log(Level.INFO, "HelloService返回到结果:" + str);
 		return str;
 	}
 }
--- dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
@@ -17,12 +17,12 @@ public class ShAction {
 
 	public String say(String name) {
                 Logger log = Logger.getLogger("com"); 
-		log.info(Level.INFO, "HelloAction接收到请求:", name);
+		log.log(Level.INFO, "HelloAction接收到请求:", name);
 		String str = "<h1>这是Dubbo 消费者端(springboot)</h1>";
 		str += "<h2>这里是上海站的页面</h2>";
                 str+="<h2>这里是上海站新加入的功能，灰度测试</h2>";
 		str += helloService.hello(name);
-		log.info(Level.INFO, "HelloService返回到结果:", str);
+		log.log(Level.INFO, "HelloService返回到结果:", str);
 		return str;
 	}
 }
